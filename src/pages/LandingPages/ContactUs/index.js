39e50e3177247836abaa8f9ecf19bd4e7408d7aa
Dhinakar_Vxceed <Dhinakar.C@vxceed.com>
2024-09-12 17:26:24 +0530

    Update index.js

--- src/pages/LandingPages/ContactUs/index.js
+++ src/pages/LandingPages/ContactUs/index.js
@@ -28,6 +28,17 @@ function ContactUs() {
         sticky
       />
       <Grid container spacing={3} alignItems="center">
+      <Grid item xs={12} lg={6}>
+          <MKBox
+            display={{ xs: "none", lg: "flex" }}
+            width="calc(100% - 2rem)"
+            height="calc(100vh - 2rem)"
+            borderRadius="lg"
+            ml={2}
+            mt={2}
+            sx={{ backgroundImage: `url(${bgImage})` }}
+          />
+        </Grid>
         <Grid
           item
           xs={12}
